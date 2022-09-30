c90c439dc15d5880a3edc20408dc55c69938a776
Willem Poortman <wpoortman@users.noreply.github.com>
2022-09-30 10:35:36 +0200

    Feature/magewire var children (#57)
    * [feature/magewire-var-children]:+ Bugfix to now properly hand over the  var to children
    
    * [feature/magewire-var-children]:+ Bugfix to now properly hand over the  var to children 2/2
    
    Co-authored-by: Willem Poortman <wp@hyva.io>

--- src/Plugin/Magento/Framework/View/TemplateEngine/Php.php
+++ src/Plugin/Magento/Framework/View/TemplateEngine/Php.php
@@ -12,9 +12,24 @@ use Magento\Framework\DataObject;
 use Magento\Framework\View\Element\BlockInterface;
 use Magento\Framework\View\TemplateEngine\Php as Subject;
 use Magewirephp\Magewire\Component;
+use Magewirephp\Magewire\Model\LayoutRenderLifecycle;
 
 class Php
 {
+    protected LayoutRenderLifecycle $layoutRenderLifecycle;
+
+    /** @var Component[] $registry */
+    private array $registry = [];
+
+    /**
+     * @param LayoutRenderLifecycle $layoutRenderLifecycle
+     */
+    public function __construct(
+        LayoutRenderLifecycle $layoutRenderLifecycle
+    ) {
+        $this->layoutRenderLifecycle = $layoutRenderLifecycle;
+    }
+
     /**
      * Automatically assign $magewire as template Block variable.
      *
@@ -35,6 +50,14 @@ class Php
 
             if ($magewire instanceof Component) {
                 $dictionary['magewire'] = $magewire;
+                $this->registry[$magewire->name] = $magewire;
+            }
+        } elseif (count($this->registry) !== 0) {
+            $views  = $this->layoutRenderLifecycle->getViews();
+            $latest = array_search(null, array_reverse($views), true);
+
+            if (is_string($latest) && array_key_exists($latest, $this->registry) && $block instanceof DataObject) {
+                $dictionary['magewire'] = $this->registry[$latest];
             }
         }
 
