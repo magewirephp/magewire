a083d54773f88e274af1dbc042e5a3a72ad5eb40
Willem Poortman <mail@wpoortman.nl>
2022-11-08 16:00:41 +0100

    [feature/redirect-to-external]:+ redirect hydrator improvement for external urls

--- src/Model/Hydrator/Redirect.php
+++ src/Model/Hydrator/Redirect.php
@@ -8,6 +8,8 @@
 
 namespace Magewirephp\Magewire\Model\Hydrator;
 
+use Laminas\Uri\UriFactory;
+use Magento\Framework\Url\HostChecker;
 use Magento\Framework\UrlInterface;
 use Magewirephp\Magewire\Component;
 use Magewirephp\Magewire\Model\HydratorInterface;
@@ -17,15 +19,18 @@ use Magewirephp\Magewire\Model\ResponseInterface;
 class Redirect implements HydratorInterface
 {
     protected UrlInterface $builder;
+    protected HostChecker $hostChecker;
 
     /**
-     * Redirect constructor.
      * @param UrlInterface $builder
+     * @param HostChecker $hostChecker
      */
     public function __construct(
-        UrlInterface $builder
+        UrlInterface $builder,
+        HostChecker $hostChecker
     ) {
         $this->builder = $builder;
+        $this->hostChecker = $hostChecker;
     }
 
     /**
@@ -43,14 +48,28 @@ class Redirect implements HydratorInterface
      */
     public function dehydrate(Component $component, ResponseInterface $response): void
     {
-        if ($redirect = $component->getRedirect()) {
-            $url = $redirect->getUrl();
+        $redirect = $component->getRedirect();
 
-            if ($redirect->hasParams()) {
-                $url = $this->builder->getUrl($url, $redirect->getParams());
-            }
-
-            $response->effects['redirect'] = $url;
+        if ($redirect === null) {
+            return;
         }
+
+        $url = $redirect->getUrl();
+
+        if (strncmp('www.', $url, 4) === 0) {
+            $url = ($redirect->isSecure() ? 'https://' : 'http://') . str_replace('www.', '', $url);
+        }
+
+        $parse = UriFactory::factory($url);
+
+        if ($this->hostChecker->isOwnOrigin($parse->toString())) {
+            $parse = UriFactory::factory(
+                $this->builder->getUrl(ltrim($url, '\/'), $redirect->hasParams() ? $redirect->getParams() : null)
+            );
+        } elseif ($redirect->hasParams() && count($parse->getQueryAsArray()) === 0) {
+            $parse->setQuery($redirect->getParams());
+        }
+
+        $response->effects['redirect'] = $parse->toString();
     }
 }
