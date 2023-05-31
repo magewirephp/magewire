a878b78ce8e4f5bddbd9456837c22a23ab0c49cd
Willem Poortman <git@wpoortman.nl>
2023-05-31 10:58:02 +0200

    Code review changes

--- src/Setup/Patch/Data/EnableCache.php
+++ src/Setup/Patch/Data/EnableCache.php
@@ -1,12 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Willem Poortman 2021-present. All rights reserved.
  *
  * Please read the README and LICENSE files for more
  * details on copyrights and license information.
  */
-
-declare(strict_types=1);
 
 namespace Magewirephp\Magewire\Setup\Patch\Data;
 
