97b6c2dd3aef1a0a51fce2089f143c182176e1ff
Willem Poortman <git@wpoortman.nl>
2025-08-17 13:23:58 +0200

    wip

--- src/Model/Magewire/Notifier/NotificationStateEnum.php
+++ src/Model/Magewire/Notifier/NotificationStateEnum.php
@@ -12,14 +12,28 @@ namespace Magewirephp\Magewire\Model\Magewire\Notifier;
 
 enum NotificationStateEnum: string
 {
+    // IMPORTANT: sort order does matter to determine the state level.
     case IDLE = 'idle';
     case RUNNING = 'running';
+    case HOLD = 'hold';
     case FAILED = 'failed';
     case SUCCEEDED = 'succeeded';
     case TERMINATED = 'terminated';
+    case RECOVERED = 'recovered';
+
+    public function getState(): string
+    {
+        return $this->value;
+    }
 
     public function getCssClass(): string
     {
         return 'state-' . $this->value;
+    }
+
+    public function getLevel(): int
+    {
+        $level = array_search($this, self::cases(), true);
+        return $level !== false ? $level : 0;
     }
 }
