cacd21c1ec302960f3d4c43bc6de2ffd48c3135a
Jeroen Boersma <JeroenBoersma@users.noreply.github.com>
2026-03-26 12:17:33 +0100

    Bugfix: DI is compiling (#204)

---
+++ lib/registration.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © W. Poortman 2021-present. All rights reserved.
+ *
+ * Please read the README and LICENSE files for more
+ * details on copyrights and license information..
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::LIBRARY,
+    'Magewirephp_Magewire/lib',
+
+    /*
+     * At the time of writing, the `dist` folder was manually added as the `path` for the library registration.
+     * Naturally, this is variable thanks to the  portman.config.php configuration.
+     *
+     * Therefore, it is important to be aware that if the `directories.output` path changes, the path here must
+     * also be updated. The same applies to the fact that if the module path changes, this must also be taken
+     * into account, as we assume the path in which this file is located.
+     */
+    __DIR__
+);
