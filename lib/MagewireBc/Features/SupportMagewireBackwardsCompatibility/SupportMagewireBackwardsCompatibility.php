1b32bd74004acae09be97aad825e0f64380324b5
Willem Poortman <git@wpoortman.nl>
2026-04-10 13:13:17 +0200

    wip

--- lib/MagewireBc/Features/SupportMagewireBackwardsCompatibility/SupportMagewireBackwardsCompatibility.php
+++ lib/MagewireBc/Features/SupportMagewireBackwardsCompatibility/SupportMagewireBackwardsCompatibility.php
@@ -11,11 +11,32 @@ declare(strict_types=1);
 
 namespace Magewirephp\Magewire\Features\SupportMagewireBackwardsCompatibility;
 
+use Magento\Framework\View\Element\AbstractBlock;
+use Magewirephp\Magewire\Component;
 use Magewirephp\Magewire\ComponentHook;
 use Magewirephp\Magewire\Mechanisms\HandleComponents\ComponentContext;
+use Magewirephp\Magewire\Mechanisms\HandleRequests\ComponentRequestContext;
+use function Magewirephp\Magewire\on;
 
 class SupportMagewireBackwardsCompatibility extends ComponentHook
 {
+    public function provide()
+    {
+        on('magewire:component:reconstruct', function (ComponentRequestContext $componentRequestContext) {
+            return function (AbstractBlock $block) use ($componentRequestContext) {
+                $component = $block->getData('magewire');
+
+                if ($component instanceof Component) {
+                    $snapshot = $componentRequestContext->getSnapshot();
+
+                    $component->getRequest()->setServerMemo([
+                        'data' => $snapshot->getData()
+                    ]);
+                }
+            };
+        });
+    }
+
     public function dehydrate(ComponentContext $context): void
     {
         $bc = [
