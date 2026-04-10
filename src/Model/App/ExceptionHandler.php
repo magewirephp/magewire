d8d9367d6e89a1df2b5059d7ca66d63c311a1acb
Ryan Hissey <ryan@hyva.io>
2026-04-10 15:00:24 +0100

    WIP - Error Handling Modal

--- src/Model/App/ExceptionHandler.php
+++ src/Model/App/ExceptionHandler.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Magewirephp\Magewire\Model\App;
 
 use Exception;
+use Magento\Framework\App\Response\HttpInterface as HttpResponseInterface;
 use Magento\Framework\App\State as ApplicationState;
 use Magento\Framework\View\Element\AbstractBlock;
 
@@ -18,6 +19,57 @@ class ExceptionHandler extends AbstractExceptionHandler
     function __construct(
         private readonly ApplicationState $state
     ) {
+    }
+
+    function handle(Exception $exception, bool $subsequent = false): Exception|callable
+    {
+        if (! $subsequent) {
+            return parent::handle($exception, $subsequent);
+        }
+
+        if ($this->state->getMode() === ApplicationState::MODE_DEVELOPER) {
+            return static function (HttpResponseInterface $response) use ($exception): HttpResponseInterface {
+                $original = $exception;
+
+                while ($original->getPrevious() !== null) {
+                    $original = $original->getPrevious();
+                }
+
+                $data = [
+                    'magewire_error' => true,
+                    'type' => $original::class,
+                    'message' => $original->getMessage(),
+                    'code' => $original->getCode(),
+                    'file' => $original->getFile(),
+                    'line' => $original->getLine(),
+                    'trace' => array_map(static function (array $frame): array {
+                        return [
+                            'file' => $frame['file'] ?? null,
+                            'line' => $frame['line'] ?? null,
+                            'class' => $frame['class'] ?? null,
+                            'function' => $frame['function'] ?? null,
+                        ];
+                    }, array_slice($original->getTrace(), 0, 30)),
+                ];
+
+                $response->setBody(json_encode($data, JSON_UNESCAPED_SLASHES));
+                $response->setHttpResponseCode(500);
+
+                return $response;
+            };
+        }
+
+        return static function (HttpResponseInterface $response): HttpResponseInterface {
+            $data = [
+                'magewire_error' => true,
+                'message' => 'An unexpected error occurred while processing your request.',
+            ];
+
+            $response->setBody(json_encode($data, JSON_UNESCAPED_SLASHES));
+            $response->setHttpResponseCode(500);
+
+            return $response;
+        };
     }
 
     function handleWithBlock(AbstractBlock $block, Exception $exception, bool $subsequent = false): AbstractBlock
