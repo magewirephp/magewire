2a2b2568b23c3c29b17e65d5af0271cdf8476dd3
Willem Poortman <git@wpoortman.nl>
2026-06-02 08:12:12 +0200

    chore: JS-lib rebuild

--- src/view/base/web/js/magewire.js
+++ src/view/base/web/js/magewire.js
@@ -5984,7 +5984,7 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   }
   var module_default5 = src_default5;
 
-  // node_modules/@alpinejs/resize/dist/module.esm.js
+  // ../alpine/packages/resize/dist/module.esm.js
   function src_default6(Alpine3) {
     Alpine3.directive("resize", Alpine3.skipDuringClone((el, { value, expression, modifiers }, { evaluateLater: evaluateLater2, cleanup: cleanup2 }) => {
       let evaluator = evaluateLater2(expression);
