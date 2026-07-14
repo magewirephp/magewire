ae929f8830c387a6bcce30e3f3dd966bc6f7e01b
Ryan Hissey <ryan@hissey.dev>
2026-07-14 14:52:40 +0100

    accept Phrase messages in SilentException constructor
      Form::validate() throws ValidationException/AcceptableException with a                                                                                                                         
      Magento\Framework\Phrase from __(). These exceptions extend native                                                                                                                             
      \Exception (string message), so under declare(strict_types=1) a Phrase                                                                                                                         
      was rejected with a TypeError.                                                                                                                                                                 
                                                                                                                                                                                                     
      Widen SilentException::__construct() to accept string|Phrase and cast                                                                                                                          
      once, fixing the whole AcceptableException/ValidationException family at                                                                                                                       
      the root instead of casting at each call site.

--- src/Exceptions/SilentException.php
+++ src/Exceptions/SilentException.php
@@ -12,7 +12,16 @@ declare(strict_types=1);
 namespace Magewirephp\Magewire\Exceptions;
 
 use Exception;
+use Magento\Framework\Phrase;
+use Throwable;
 
 class SilentException extends Exception
 {
+    public function __construct(
+        string|Phrase $message = '',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct((string) $message, $code, $previous);
+    }
 }
